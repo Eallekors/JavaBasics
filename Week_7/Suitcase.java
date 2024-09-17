89d71f80aadb1468eac5b5b90497e5de0526c2b1
Eallekors <Erkxalle@gmail.com>
2024-09-17 11:18:26 +0300

    EX4.1

---
+++ Week_7/Suitcase.java
@@ -0,0 +1,27 @@
+package Week_7;
+
+import java.util.ArrayList;
+
+public class Suitcase {
+    ArrayList<Thing> things = new ArrayList<Thing>();
+
+    public Suitcase(int weight) {
+    }
+
+    public void addThing(Thing thing){
+        things.add(thing);
+    }
+
+    public String toString(){
+        return  "test" ;
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ Week_7/Thing.java
@@ -0,0 +1,19 @@
+package Week_7;
+
+public class Thing {
+    String Inname;
+    int Inweight;
+    public Thing(String name, int weight) {
+        Inname = name;
+        Inweight = weight;
+    }
+    public String getName() {
+        return Inname;
+    }
+    public int getWeight() {
+        return Inweight;
+    }
+    public String toString() {
+        return Inname + "(" + Inweight + " kg)";
+    }
+}
