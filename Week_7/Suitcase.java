8b128d1b7becb18f8efe49176a481f1b05f2bb2d
Eallekors <Erkxalle@gmail.com>
2024-09-17 11:35:00 +0300

    EX4.3

--- Week_7/Suitcase.java
+++ Week_7/Suitcase.java
@@ -4,24 +4,29 @@ import java.util.ArrayList;
 
 public class Suitcase {
     ArrayList<Thing> things = new ArrayList<Thing>();
-
+    private int curweight;
+    private int maxweight;
     public Suitcase(int weight) {
+        this.maxweight = weight;
     }
 
     public void addThing(Thing thing){
-        things.add(thing);
+        int currentweight = thing.getWeight();
+
+        if(curweight + thing.getWeight() < maxweight) {
+            things.add(thing);
+            curweight += thing.getWeight();
+        }
     }
 
     public String toString(){
-        return  "test" ;
+        if (things.size() == 0){
+            return "empty (0 kg)";
+        }else if(things.size() == 1){
+            return things.size() + " thing ( " + curweight + " kg)";
+        }else {
+            return things.size() + " things ( " + curweight + " kg)";
+        }
+
     }
-
-
-
-
-
-
-
-
-
 }
